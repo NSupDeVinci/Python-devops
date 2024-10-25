04878ce835ee3f201d5a9e1f418768ebfb14be65
Nassim <nassim.hasnaoui@supdevinci-edu.fr>
2024-10-25 16:43:52 +0200

    Test changement de message

--- Project/app.py
+++ Project/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Flask API! This is the new message Octobre 25"
+    return "ALLEZ L'OM"
 
 @app.route('/add', methods=['POST'])
 def add():
